2ad6fe6e71b2a055749e6e47ea313c10bc80699c
annie971101 <62496332+annie971101@users.noreply.github.com>
2020-12-07 21:12:42 -0600

    Update pix2pix.py

--- pix2pix.py
+++ pix2pix.py
@@ -77,27 +77,3 @@ def load_test_image(filname):
 
 train_dataset = tf.data.Dataset.from_tensor_slices(ts_urls)
 train_dataset = train_dataset.map(load_train_image)
-tr_urls = randurls[:train_n]
-ts_urls = randurls[train_n:n]
-
-print(len(result), len(tr_urls), len(ts_urls))
-
-#BUFFER_SIZE = 400
-#BATCH_SIZE = 1
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
-#escalar imagenes
-def resize(inimg, tgimg, height, width):
-	inimg = tf.image.resize(inimg, [height, width])
-	tgimg = tf.image.resize(tgimg, [height, width])
-	return inimg, tgimg
-
-#normalizar 
-def normalize(inimg, tgimg, height, width):
-	inimg = (inimg / 127.5) - 1
-	tgimg = (tgimg / 127.5) - 1
-	return inimg, tgimg
-
-
- 
-
